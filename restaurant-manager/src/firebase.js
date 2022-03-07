66837ecb7bf5d6e08cfacfe371666b5fd8fbaeed
yami-newbie <68190399+yami-newbie@users.noreply.github.com>
2022-03-08 02:57:51 +0700

    .

---
+++ restaurant-manager/src/firebase.js
@@ -0,0 +1,10 @@
+import firebaseConfig from "./config/firebaseConfig";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+
+export { auth, db };
